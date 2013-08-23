52f85f769f44b8fd95d02ccb52ba8a9670b331ad
spiderck <kai.ck.chen@gmail.com>
2013-08-23 09:37:07 +0800

    add class level javadoc

--- src/main/java/com/jdc/themis/dealer/dao/RefDataDAO.java
+++ src/main/java/com/jdc/themis/dealer/dao/RefDataDAO.java
@@ -6,6 +6,12 @@ import java.util.Map;
 import com.jdc.themis.dealer.domain.Menu;
 import com.jdc.themis.dealer.domain.MenuHierachy;
 
+/**
+ * Data access layer to query static reference data like menus. 
+ * 
+ * @author chen386_2000
+ *
+ */
 public interface RefDataDAO {
 
 	List<Menu> getMenuList();
